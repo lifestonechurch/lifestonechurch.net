38b161a39ab04b133199fe325f93b890ed4bcebd
Adam Harris <aharris88@users.noreply.github.com>
2015-02-14 22:39:52 -0700

    Fix assets

---
+++ assets/js/countdown_languages/french.js
@@ -0,0 +1,14 @@
+/* http://keith-wood.name/countdown.html
+   French initialisation for the jQuery countdown extension
+   Written by Keith Wood (kbwood{at}iinet.com.au) Jan 2008. */
+(function($) {
+	$.countdown.regional['fr'] = {
+		labels: ['Années', 'Mois', 'Semaines', 'Jours', 'Heures', 'Minutes', 'Secondes'],
+		labels1: ['Année', 'Mois', 'Semaine', 'Jour', 'Heure', 'Minute', 'Seconde'],
+		compactLabels: ['a', 'm', 's', 'j'],
+		whichLabels: function(amount) {
+            return (amount > 1 ? 0 : 1);
+        },
+		timeSeparator: ':', isRTL: false};
+	$.countdown.setDefaults($.countdown.regional['fr']);
+})(jQuery);
