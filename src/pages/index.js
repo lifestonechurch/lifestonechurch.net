79d2d85ad66c5f40b5bf83129ce1b865650ef404
Adam Garrett-Harris <agarrharr@users.noreply.github.com>
2017-08-27 15:56:24 -0600

    Add sermon image

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import Link from 'gatsby-link'
 import Card from '../components/Card';
 import Button from '../components/Button';
 import SermonCard from '../components/SermonCard';
+import genesis from '../images/genesis.jpg';
 
 const IndexPage = () =>
   <div>
@@ -11,23 +12,23 @@ const IndexPage = () =>
     <p>Now go build something great.</p>
     <Link to="/page-2/">Go to page 2</Link>
 
-    <SermonCard 
-      image='/assets/uploads/podcast/genesis.jpg'
-      title='Genesis 37 - Did It Have To Go This Way?' 
+    <SermonCard
+      image={genesis}
+      title='Genesis 37 - Did It Have To Go This Way?'
       date='8/20/17'
-      speaker=' Pastor Ben Helton' 
-      passage='Genesis 37' 
+      speaker=' Pastor Ben Helton'
+      passage='Genesis 37'
     />
 
-    <SermonCard 
-    image='' 
-    title="Genesis 39 - Oh no he didn't"
-    date='8/20/17'
-    speaker=' Pastor Ben Helton' 
-    passage='Genesis 37' 
+    <SermonCard
+      image={genesis}
+      title="Genesis 39 - Oh no he didn't"
+      date='8/20/17'
+      speaker=' Pastor Ben Helton'
+      passage='Genesis 37'
     />
 
-    <Card> 
+    <Card>
       <Button>I don't need no defualt</Button>
       <p>I'm a paragraph</p>
     </Card>
