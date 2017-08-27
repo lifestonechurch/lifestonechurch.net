59f65608ea30b17c3b84b1d3845601d48f9d9772
Adam Garrett-Harris <agarrharr@users.noreply.github.com>
2017-08-27 16:43:22 -0600

    Add google font and move Header component

---
+++ src/components/Header.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const Header = () =>
+  <div
+    style={{
+      background: 'green',
+      marginBottom: '1.45rem',
+    }}
+  >
+    <div
+      style={{
+        margin: '0 auto',
+        maxWidth: 960,
+        padding: '1.45rem 1.0875rem',
+      }}
+    >
+      <h1 style={{ margin: 0 }}>
+        <Link
+          to="/"
+          style={{
+            color: 'white',
+            textDecoration: 'none',
+          }}
+        >
+          Lifestone Church
+        </Link>
+      </h1>
+    </div>
+  </div>
+
+export default Header
