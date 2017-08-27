95d0999a6c9941d6db22eed4c0373147c60c1076
Adam Garrett-Harris <agarrharr@users.noreply.github.com>
2017-08-27 17:26:10 -0600

    Add Container component

---
+++ src/components/Container.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import './Container.css'
+
+const Container = ({children}) =>
+  <div className="Container">
+    {children}
+  </div>
+
+export default Container
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 import Header from '../components/Header';
+import Container from '../components/Container';
 
 import './index.css'
 
@@ -16,9 +17,9 @@ const TemplateWrapper = ({children}) =>
     />
       <link href='http://fonts.googleapis.com/css?family=Lato:400,700|Droid+Serif:400,700,400italic' rel='stylesheet' type='text/css'/>
     <Header />
-    <div>
+    <Container>
       {children()}
-    </div>
+    </Container>
   </div>
 
 TemplateWrapper.propTypes = {
