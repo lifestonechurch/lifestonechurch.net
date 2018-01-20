6fc8f59d9245fcaee020568f85f67dadbbd01c9a
Adam Garrett-Harris <agarrharr@users.noreply.github.com>
2018-01-20 15:59:08 -0700

    Clean up homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,28 +9,6 @@ import EventCard from '../components/EventCard';
 const IndexPage = () => (
   <div>
     <p>TODO: Home Page</p>
-
-    <SermonCard
-      image={genesis}
-      title="Genesis 37 - Did It Have To Go This Way?"
-      date="8/20/17"
-      speaker=" Pastor Ben Helton"
-      passage="Genesis 37"
-    />
-
-    <EventCard
-      title="Ice Cream Social"
-      date="August 25"
-      linkTo="events/ice-cream"
-    />
-
-    <Card>
-      <Button>I'm a button</Button>
-      <p>I'm a paragraph</p>
-    </Card>
-
-    <Button linkTo="http://www.google.com">I am a button!</Button>
-    <Button linkTo="contact">No, I am a button!</Button>
   </div>
 );
 
