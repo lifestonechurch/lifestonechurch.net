15849174f84f03e9739affe8ff47d4292bcdff77
Adam Garrett-Harris <agarrharr@users.noreply.github.com>
2018-03-22 11:30:00 -0600

    Fix menu scrolling issue

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import Link from 'gatsby-link';
-import styled from 'react-emotion';
-import Burger from './Burger';
-import * as COLORS from '../constants/colors';
-import logo from '../../assets/logo2.png';
+import React from "react";
+import PropTypes from "prop-types";
+import Link from "gatsby-link";
+import styled from "react-emotion";
+import Burger from "./Burger";
+import * as COLORS from "../constants/colors";
+import logo from "../../assets/logo2.png";
 
 const MIN_DESKTOP_SIZE = 1163;
 
@@ -41,6 +41,7 @@ const DesktopNav = styled.ul`
     display: none;
   }
 `;
+
 const DesktopSubNav = styled.ul`
   display: flex;
   flex-direction: column;
@@ -148,46 +149,52 @@ const Image = styled.img`
   height 45px;
   padding: 4px 0;
   z-index: 1000;
-  display: ${props => (props.isVisible ? 'visible' : 'none')};
+  display: ${props => (props.isVisible ? "visible" : "none")};
 `;
 
 Image.propTypes = {
-  isVisible: PropTypes.bool.isRequired,
+  isVisible: PropTypes.bool.isRequired
 };
 
 class Menu extends React.Component {
   state = {
     openDesktopItem: null,
     openMobileItems: {},
-    isOpen: false,
+    isOpen: false
   };
 
   handleMobileToggle = isOpen => {
-    this.setState({ isOpen });
-    document.body.style.overflow = isOpen ? 'hidden' : 'auto';
+    this.setState({ isOpen }, () => {
+      document.body.style.overflow = isOpen ? "hidden" : "auto";
+    });
   };
 
   handleDesktopItemClick = index => {
     this.setState({
-      openDesktopItem: this.state.openDesktopItem === index ? null : index,
+      openDesktopItem: this.state.openDesktopItem === index ? null : index
     });
   };
 
   handleHomeClick = () => {
     this.setState({
-      isOpen: false,
+      isOpen: false
     });
   };
 
+  allowScroll() {
+    document.body.style.overflow = "auto";
+  }
+
   handleMobileItemClick = (item, i) => {
     const items = this.state.openMobileItems;
+    this.allowScroll();
     this.setState({
       openMobileItems: items[i]
         ? Object.keys(items)
             .filter(a => a !== i)
             .reduce((a, c) => ({ ...a, c: true }), {})
         : { ...items, [i]: true },
-      isOpen: item.children ? this.state.isOpen : false,
+      isOpen: item.children ? this.state.isOpen : false
     });
   };
 
@@ -217,7 +224,7 @@ class Menu extends React.Component {
               )}
               {a.children && (
                 <DesktopSubNav
-                  className={this.state.openDesktopItem === i ? 'open' : ''}
+                  className={this.state.openDesktopItem === i ? "open" : ""}
                 >
                   {a.children.map((b, j) => (
                     <Link
@@ -236,12 +243,17 @@ class Menu extends React.Component {
         <Mobile>
           <Burger
             isOpen={this.state.isOpen}
-            color={this.state.isOpen ? 'white' : COLORS.BRAND}
+            color={this.state.isOpen ? "white" : COLORS.BRAND}
             onClick={this.handleMobileToggle}
           />
-          <MobileContainer className={this.state.isOpen ? 'open' : ''}>
+          <MobileContainer className={this.state.isOpen ? "open" : ""}>
             <Link to="/" onClick={() => this.handleHomeClick()}>
-              <Image src={logo} alt="Logo" isVisible={this.state.isOpen} />
+              <Image
+                src={logo}
+                alt="Logo"
+                isVisible={this.state.isOpen}
+                onClick={this.allowScroll}
+              />
             </Link>
             <MobileNav>
               {navigation.map((a, i) => (
@@ -263,7 +275,7 @@ class Menu extends React.Component {
                   )}
                   {a.children && (
                     <MobileSubNav
-                      className={this.state.openMobileItems[i] ? 'open' : ''}
+                      className={this.state.openMobileItems[i] ? "open" : ""}
                     >
                       {a.children.map((b, j) => (
                         <Link
@@ -294,11 +306,11 @@ Menu.propTypes = {
       children: PropTypes.arrayOf(
         PropTypes.shape({
           name: PropTypes.string.isRequired,
-          path: PropTypes.string,
+          path: PropTypes.string
         })
-      ),
+      )
     })
-  ).isRequired,
+  ).isRequired
 };
 
 export default Menu;
