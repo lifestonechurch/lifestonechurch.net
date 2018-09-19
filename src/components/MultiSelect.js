938062c32483edbda70616a8830c469357e0e9f9
Adam Garrett-Harris <agarrharr@users.noreply.github.com>
2018-09-19 13:06:32 -0600

    Add filter for lifeGroups

---
+++ src/components/MultiSelect.js
@@ -0,0 +1,269 @@
+import React from 'react';
+import Downshift from 'downshift';
+import {
+  Menu,
+  ControllerButton,
+  Item,
+  ArrowIcon,
+  css,
+  getItems,
+} from './DownshiftComponents';
+
+import * as COLORS from '../constants/colors';
+
+class MultiDownshift extends React.Component {
+  state = { selectedItems: [] };
+
+  stateReducer = (state, changes) => {
+    switch (changes.type) {
+      case Downshift.stateChangeTypes.keyDownEnter:
+      case Downshift.stateChangeTypes.clickItem:
+        return {
+          ...changes,
+          highlightedIndex: state.highlightedIndex,
+          isOpen: true,
+        };
+      default:
+        return changes;
+    }
+  };
+
+  callOnChange = () => {
+    const { onSelect, onChange } = this.props;
+    const { selectedItems } = this.state;
+    if (onSelect) {
+      onSelect(selectedItems);
+    }
+    if (onChange) {
+      onChange(selectedItems);
+    }
+  };
+
+  handleSelection = (selectedItem, downshift) => {
+    if (this.state.selectedItems.includes(selectedItem)) {
+      this.removeItem(selectedItem, this.callOnChange);
+    } else {
+      this.addSelectedItem(selectedItem, this.callOnChange);
+    }
+  };
+
+  removeItem = (item, cb) => {
+    this.setState(({ selectedItems }) => {
+      return {
+        selectedItems: selectedItems.filter(i => i !== item),
+      };
+    }, cb);
+  };
+
+  addSelectedItem(item, cb) {
+    this.setState(
+      ({ selectedItems }) => ({
+        selectedItems: [...selectedItems, item],
+      }),
+      cb
+    );
+  }
+
+  getRemoveButtonProps = ({ onClick, item, ...props } = {}) => {
+    return {
+      onClick: e => {
+        onClick && onClick(e);
+        e.stopPropagation();
+        this.removeItem(item, this.callOnChange);
+      },
+      ...props,
+    };
+  };
+
+  getStateAndHelpers(downshift) {
+    const { selectedItems } = this.state;
+    const { getRemoveButtonProps, removeItem, callOnChange } = this;
+    return {
+      getRemoveButtonProps,
+      removeItem,
+      callOnChange,
+      selectedItems,
+      ...downshift,
+    };
+  }
+
+  render() {
+    const { render, children = render, ...props } = this.props;
+    return (
+      <Downshift
+        {...props}
+        stateReducer={this.stateReducer}
+        onChange={this.handleSelection}
+        selectedItem={null}
+      >
+        {downshift => children(this.getStateAndHelpers(downshift))}
+      </Downshift>
+    );
+  }
+}
+
+class MultiSelect extends React.Component {
+  input = React.createRef();
+  itemToString = item => (item ? item.name : '');
+  render() {
+    return (
+      <div
+        {...css({
+          display: 'flex',
+          flexDirection: 'column',
+          margin: '20px 0 50px',
+        })}
+      >
+        <MultiDownshift
+          onChange={this.props.onChange}
+          itemToString={this.itemToString}
+        >
+          {({
+            getInputProps,
+            getToggleButtonProps,
+            getMenuProps,
+            getRemoveButtonProps,
+            removeItem,
+            callOnChange,
+            isOpen,
+            inputValue,
+            selectedItems,
+            getItemProps,
+            highlightedIndex,
+            toggleMenu,
+          }) => (
+            <div style={{ width: 500, margin: 'auto', position: 'relative' }}>
+              <div
+                {...css({
+                  cursor: 'pointer',
+                  position: 'relative',
+                  borderRadius: '6px',
+                  borderTopRadius: 6,
+                  borderBottomRightRadius: isOpen ? 0 : 6,
+                  borderBottomLeftRadius: isOpen ? 0 : 6,
+                  padding: 10,
+                  paddingRight: 50,
+                  boxShadow: '0 2px 3px 0 rgba(34,36,38,.15)',
+                  borderColor: '#96c8da',
+                  borderTopWidth: '1',
+                  borderRightWidth: 1,
+                  borderBottomWidth: 1,
+                  borderLeftWidth: 1,
+                  borderStyle: 'solid',
+                })}
+                onClick={() => {
+                  toggleMenu();
+                  !isOpen && this.input.current.focus();
+                }}
+              >
+                <div
+                  {...css({
+                    display: 'flex',
+                    flexWrap: 'wrap',
+                    alignItems: 'center',
+                  })}
+                >
+                  {selectedItems.length > 0
+                    ? selectedItems.map(item => (
+                        <div
+                          key={item.id}
+                          {...css({
+                            margin: 2,
+                            paddingTop: 2,
+                            paddingBottom: 2,
+                            paddingLeft: 8,
+                            paddingRight: 8,
+                            display: 'inline-block',
+                            wordWrap: 'none',
+                            backgroundColor: COLORS.BRAND,
+                            color: 'white',
+                            borderRadius: 2,
+                          })}
+                        >
+                          <div
+                            {...css({
+                              display: 'grid',
+                              gridGap: 6,
+                              gridAutoFlow: 'column',
+                              alignItems: 'center',
+                            })}
+                          >
+                            <span>{item.name}</span>
+                            <button
+                              {...getRemoveButtonProps({ item })}
+                              {...css({
+                                cursor: 'pointer',
+                                lineHeight: 0.8,
+                                border: 'none',
+                                backgroundColor: 'transparent',
+                                padding: '0',
+                                fontSize: '16px',
+                                color: 'white',
+                              })}
+                            >
+                              𝘅
+                            </button>
+                          </div>
+                        </div>
+                      ))
+                    : null}
+                  <input
+                    {...getInputProps({
+                      ref: this.input,
+                      onKeyUp(event) {
+                        if (event.key === 'Backspace' && !inputValue) {
+                          removeItem(
+                            selectedItems[selectedItems.length - 1],
+                            callOnChange
+                          );
+                        }
+                      },
+                      ...css({
+                        border: 'none',
+                        marginLeft: 6,
+                        flex: 1,
+                        fontSize: 14,
+                        minHeight: 27,
+                        backgroundColor: 'inherit',
+                      }),
+                    })}
+                  />
+                </div>
+                <ControllerButton
+                  {...getToggleButtonProps({
+                    // prevents the menu from immediately toggling
+                    // closed (due to our custom click handler above).
+                    onClick(event) {
+                      event.stopPropagation();
+                    },
+                  })}
+                >
+                  <ArrowIcon isOpen={isOpen} />
+                </ControllerButton>
+              </div>
+              <Menu {...getMenuProps({ isOpen })}>
+                {isOpen
+                  ? getItems(inputValue).map((item, index) => (
+                      <Item
+                        key={item.id}
+                        {...getItemProps({
+                          item,
+                          index,
+                          isActive: highlightedIndex === index,
+                          isSelected: selectedItems.includes(item),
+                        })}
+                      >
+                        {item.name}
+                      </Item>
+                    ))
+                  : null}
+              </Menu>
+            </div>
+          )}
+        </MultiDownshift>
+      </div>
+    );
+  }
+}
+
+export default MultiSelect;
--- src/pages/connect/lifegroups/index.js
+++ src/pages/connect/lifegroups/index.js
@@ -4,13 +4,18 @@ import { graphql } from 'gatsby';
 import Img from 'gatsby-image';
 
 import Layout from '../../../components/layout';
-import { H1, H2 } from '../../../components/headers';
+import { H1, H2, H3 } from '../../../components/headers';
 import Banner from '../../../components/Banner';
 import LifeGroup from '../../../components/LifeGroup';
 import Breadcrumbs from '../../../components/Breadcrumbs';
 import VideoPlayer from '../../../components/VideoPlayer';
+import MultiSelect from '../../../components/MultiSelect';
 
 const title = 'LifeGroups';
+
+const Center = styled.div`
+  text-align: center;
+`;
 
 const CardContainer = styled.div`
   display: flex;
@@ -41,85 +46,114 @@ const sortByDayOfWeek = (a, b) => {
   return days[a.node.day] - days[b.node.day];
 };
 
-const Page = ({ data }) => {
-  const lifegroups = data.allContentfulSmallGroup.edges;
+class Page extends React.Component {
+  state = {
+    selectedDays: [],
+  };
 
-  return (
-    <Layout>
-      <Breadcrumbs
-        path={[{ title: 'Home', url: '/' }, { title: 'Connect' }]}
-        title={title}
-      />
-      <H1>{title}</H1>
+  handleLifeGroupDayChange = days => {
+    this.setState(prevState => ({
+      ...prevState,
+      selectedDays: days.map(d => d.name),
+    }));
+  };
 
-      <Image>
-        <Img sizes={data.lifegroupImage.childImageSharp.sizes} />
-      </Image>
+  render() {
+    const { data } = this.props;
+    const { selectedDays } = this.state;
 
-      <Banner>
-        <H2>Preview Fall Series</H2>
-      </Banner>
+    const lifegroups = data.allContentfulSmallGroup.edges;
 
-      <VideoPlayer
-        url="https://player.rightnow.org/241480"
-        maxWidth={600}
-        title="The Good Book LifeGroup Series"
-      />
+    return (
+      <Layout>
+        <Breadcrumbs
+          path={[{ title: 'Home', url: '/' }, { title: 'Connect' }]}
+          title={title}
+        />
+        <H1>{title}</H1>
 
-      <Banner>Check Out Groups</Banner>
+        <Image>
+          <Img sizes={data.lifegroupImage.childImageSharp.sizes} />
+        </Image>
 
-      <CardContainer>
-        {lifegroups.sort(sortByDayOfWeek).map(({ node }, index, array) => (
-          <div key={node.id}>
-            <LifeGroup
-              name={node.name}
-              description={node.description && node.description.description}
-              day={node.day}
-              time={node.time}
-              address={node.address || ''}
-              hosts={
-                node.hosts
-                  ? {
-                      id: node.hosts[0].id,
-                      name: node.hosts[0].name,
-                      description: node.hosts[0].description
-                        ? node.hosts[0].description.description
-                        : '',
-                      photoSizes: node.hosts[0].photo
-                        ? node.hosts[0].photo.sizes
-                        : undefined,
-                      photoTitle: node.hosts[0].photo
-                        ? node.hosts[0].photo.title
-                        : '',
-                    }
-                  : {}
-              }
-              leaders={
-                node.leaders
-                  ? {
-                      id: node.leaders[0].id,
-                      name: node.leaders[0].name,
-                      description: node.leaders[0].description
-                        ? node.leaders[0].description.description
-                        : '',
-                      photoSizes: node.leaders[0].photo
-                        ? node.leaders[0].photo.sizes
-                        : undefined,
-                      photoTitle: node.leaders[0].photo
-                        ? node.leaders[0].photo.title
-                        : '',
-                    }
-                  : {}
-              }
-              contact={node.fields.contactFormatted}
-              hasChildcare={node.hasChildcare}
-            />
-          </div>
-        ))}
-      </CardContainer>
-    </Layout>
-  );
-};
+        <Banner>
+          <H2>Preview Our Fall Series</H2>
+        </Banner>
+
+        <VideoPlayer
+          url="https://player.rightnow.org/241480"
+          maxWidth={600}
+          title="The Good Book LifeGroup Series"
+        />
+
+        <Banner>Check Out Our Groups</Banner>
+
+        <Center>
+          <H3>Which days work best for you?</H3>
+        </Center>
+
+        <MultiSelect onChange={this.handleLifeGroupDayChange} />
+
+        <CardContainer>
+          {lifegroups
+            .filter(
+              lifegroup =>
+                selectedDays.includes(lifegroup.node.day) ||
+                selectedDays.length === 0
+            )
+            .sort(sortByDayOfWeek)
+            .map(({ node }, index, array) => (
+              <div key={node.id}>
+                <LifeGroup
+                  name={node.name}
+                  description={node.description && node.description.description}
+                  day={node.day}
+                  time={node.time}
+                  address={node.address || ''}
+                  hosts={
+                    node.hosts
+                      ? {
+                          id: node.hosts[0].id,
+                          name: node.hosts[0].name,
+                          description: node.hosts[0].description
+                            ? node.hosts[0].description.description
+                            : '',
+                          photoSizes: node.hosts[0].photo
+                            ? node.hosts[0].photo.sizes
+                            : undefined,
+                          photoTitle: node.hosts[0].photo
+                            ? node.hosts[0].photo.title
+                            : '',
+                        }
+                      : {}
+                  }
+                  leaders={
+                    node.leaders
+                      ? {
+                          id: node.leaders[0].id,
+                          name: node.leaders[0].name,
+                          description: node.leaders[0].description
+                            ? node.leaders[0].description.description
+                            : '',
+                          photoSizes: node.leaders[0].photo
+                            ? node.leaders[0].photo.sizes
+                            : undefined,
+                          photoTitle: node.leaders[0].photo
+                            ? node.leaders[0].photo.title
+                            : '',
+                        }
+                      : {}
+                  }
+                  contact={node.fields.contactFormatted}
+                  hasChildcare={node.hasChildcare}
+                />
+              </div>
+            ))}
+        </CardContainer>
+      </Layout>
+    );
+  }
+}
 
 export default Page;
 
