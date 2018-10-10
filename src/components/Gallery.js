32ef6d320dadb65e9a3fc045e1fc2c76f6df7972
Phil <35127144+flurmbo@users.noreply.github.com>
2018-10-10 13:02:44 -0500

    Fix image gallery css (#515)
    Closes #514

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,10 +1,30 @@
 import React from 'react';
 import ImageGallery from 'react-image-gallery';
+import * as COLORS from '../constants/colors';
+
 
 import 'react-image-gallery/styles/css/image-gallery.css';
 
+function renderImage(item) {
+    return (
+      <div className='image-gallery-image'>
+        <img src={item.original}
+          style={{height: '100vh',
+                  width: '100vw',
+                  objectFit: 'scale-down',
+                  fontFamily: "'object-fit: scale-down'",
+                  backgroundColor: COLORS.LIGHT_GRAY}}
+        />
+      </div>
+    );
+}
+
 const Gallery = ({ images }) => (
-  <ImageGallery items={images} showThumbnails={false} />
+  <ImageGallery items={images}
+    showThumbnails={false}
+    renderItem={renderImage}
+  />
+    
 );
 
 export default Gallery;
