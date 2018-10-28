8850e38d86460f0a3d867e15bbefcc19f6bc93b3
Adam Garrett-Harris <agarrharr@users.noreply.github.com>
2018-10-27 23:02:51 -0600

    Add faq

--- src/pages/lifegroup-leaders/faq.js
+++ src/pages/lifegroup-leaders/faq.js
@@ -1,11 +1,139 @@
 import React from 'react';
 
+import { H2 } from '../../components/headers';
 import LifeGroupLeaderLayout from './LifeGroupLeaderLayout';
 
 const Page = () => (
   <LifeGroupLeaderLayout>
     <div>
-      <h2>FAQ</h2>
+      <H2>FAQ</H2>
+      <ol>
+        <li>
+          <p>Can my group meet at the church?</p>
+
+          <p>
+            LifeGroups are meant for doing life together. When groups meet at
+            the church it begins to reflect another church service which can
+            break down some of the intimacy that comes with meeting in homes. If
+            your group becomes too big to meet in a house then it is time to
+            multiply, rather than finding a bigger space. Furthermore, meeting
+            at the church ties up the building in case it needs to be used for
+            other purposes as well. If something comes up and a group needs to
+            meet there for a week or two that is okay, but we do not want groups
+            meeting there as a long term solution.
+          </p>
+        </li>
+
+        <li>
+          <p>
+            Can I bring in a Non-Lifestone person to speak or talk on something
+            during group time?
+          </p>
+
+          <p>
+            Everything that is taught needs to be approved by the leadership.
+            Even if it is someone we all know and love, please ask permission
+            first. If there are theological questions that need to be answered
+            then please go up the ladder of leadership which is Ashley Smith,
+            who oversees all LifeGroups, and then Pastor Ben.
+          </p>
+        </li>
+
+        <li>
+          <p>
+            How do I care for my whole group when I naturally connect to only a
+            few?
+          </p>
+
+          <p>
+            At times it can be a struggle to care for people. There will be some
+            people that you will connect with naturally and some people that you
+            won't connect with. The answer isn't equal time, but it is equal
+            care. We are all different and are naturally drawn to certain
+            personalities. This isn't sinful or bad leadership, it is just life
+            and how we build community for ourselves. As a leader, we have to be
+            aware of our tendency to connect to only a few and constantly ask
+            ourselves "How are we caring for others in our group?". Remember,
+            there are five common ways that people receive love: quality time,
+            acts of service, receiving gifts, touch, words of encouragement. Try
+            some different methods out on those you have a harder time
+            connecting with and see how they respond. You will notice that they
+            will likely respond to one greater than the others.
+          </p>
+        </li>
+
+        <li>
+          <p>
+            I have people in my group that I think will be great leaders. What
+            do I do?
+          </p>
+
+          <p>
+            As shepherd, we should always be observant to what the Lord is doing
+            in the lives of our group members and praying for the next
+            generation of leaders. When you identify a potential lifegroup
+            leader then start giving them more responsibility in your group.
+            Perhaps you could ask them to do the opening prayer, ice breaker, or
+            plan an event for the group. Once you gained confidence in your
+            potential leaders, please share their progress with Ashley Smith
+            (LifeGroup Coach). We will then move forward with additional
+            conversations and training.
+          </p>
+        </li>
+
+        <li>
+          <p>My group is getting too big. What do I do?</p>
+
+          <p>
+            Multiply! We use the word 'multiply' on purpose. We want to refrain
+            from using the word 'dividing' because with division there is no
+            growth. By multiplying, we are providing more space for those that
+            are needing a lifegroup family. At Lifestone, we want to see our
+            growth come from inviting new people. As your group grows larger,
+            your job as a lifegroup leader is to continually be evaluating new
+            potential leaders. Part of this process is to prepare and equip them
+            so that they can lead a group when the time is right. Having these
+            new leaders ready will allow the multiplication process to occur
+            more naturally. It takes purposeful relationships to build up new
+            leaders and to keep multiplication fresh on your group's mind. (This
+            is an overview. Please talk to your Lifegroup Coach - Ashley Smith
+            before talking to anyone about leading a lifegroup)
+          </p>
+        </li>
+
+        <li>
+          <p>
+            Why can't my group choose their own study or not have a lesson at
+            all.
+          </p>
+
+          <p>
+            We need to start groups with some basic structure as we are training
+            up new leaders. Having a set lesson is a reproducible way to equip
+            them. We believe in giving the Holy Spirit permission to take the
+            group where it needs to go regardless of where you start. It is
+            similar to Sunday morning. Ben prepares a sermon and the worship
+            team rehearses songs so everyone knows the flow of the service, but
+            room is always left for the Holy Spirit to work. For example, the
+            Holy Spirit may prompt the band may do an extra song that wasn't on
+            the set list or someone may spontaneously come forward to get
+            baptized (as we have recently seen, Praise Jesus!). We fully believe
+            in and support our lifegroup leaders and understand that the lesson
+            will be taught through the filter of their life experiences and the
+            needs of their group. You are a highly valued shepherd! The lesson
+            isn't intended to be a rigid structure that creates uniformity in
+            every single group. Instead, it serves as a guide to open the Bible
+            and share your life. It is also good to remember that some
+            connections should happen outside of scheduled lifegroup time. There
+            will be seasons where the leaders can choose a study for their
+            group. This can be a great way to produce great fruit in both the
+            leader and their lifegroup. Whichever season the church is in, we
+            ask that you trust us in our decisions for curriculum. We value each
+            of your individual opinions and it is our desire to hear from the
+            Lord on what He is wanting to teach Lifestone as a whole.
+          </p>
+        </li>
+      </ol>
     </div>
   </LifeGroupLeaderLayout>
 );
