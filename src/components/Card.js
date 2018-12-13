lifestonechurch/lifestonechurch.net

commit 0bfd94b56db07cb2565bef7b6ccfd1a715d91b0c

Fix lifeGroup day selector
Closes #552

- It was letting you select more than one day
- The menu was also off-center from the input

src/components/MultiSelect.js
@@ -57,9 +57,12 @@ class MultiDownshift extends React.Component {
 
   addSelectedItem(item, cb) {
     this.setState(
-      ({ selectedItems }) => ({
-        selectedItems: [...selectedItems, item],
-      }),
+      ({ selectedItems }) =>
+        console.log(selectedItems) || {
+          selectedItems: selectedItems.map(item => item.id).includes(item.id)
+            ? selectedItems
+            : [...selectedItems, item],
+        },
       cb
     );
   }

src/utils/typography.js
@@ -1,11 +1,11 @@
 import Typography from 'typography';
 import theme from 'typography-theme-bootstrap';
 
-// theme.overrideThemeStyles = ({ rhythm }, options, styles) => ({
-//   li: {
-//     padding: 0,
-//   },
-// });
+theme.overrideThemeStyles = ({ rhythm }, options, styles) => ({
+  ul: {
+    marginLeft: 0,
+  },
+});
 
 const typography = new Typography(theme);
 const { rhythm, scale } = typography;

commit be6eca38758c2d8bc82edce5d022d5b6f3be06e2

Make lifeGroup cards responsive again
- Make the lifeGroup day picker white
- Fix ul left margin bug caused by 0bfd94b56db07

src/components/Card.js
@@ -3,8 +3,6 @@ import styled from 'react-emotion';
 
 const Container = styled.div`
   position: relative;
-  margin-top: 20px;
-  margin-bottom: 20px;
   color: rgba(0, 0, 0, 0.6);
   background: rgb(255, 255, 255) none repeat scroll 0% 0%;
   border-radius: 4px;

src/components/DownshiftComponents.js
@@ -85,6 +85,7 @@ const Label = styled('label')({
 const BaseMenu = styled('ul')(
   {
     padding: 0,
+    marginLeft: 0,
     marginTop: 0,
     position: 'absolute',
     backgroundColor: 'white',

src/components/MultiSelect.js
@@ -156,6 +156,7 @@ class MultiSelect extends React.Component {
                   padding: 10,
                   paddingRight: 50,
                   boxShadow: '0 2px 3px 0 rgba(34,36,38,.15)',
+                  background: 'white',
                   borderColor: '#96c8da',
                   borderTopWidth: '1',
                   borderRightWidth: 1,
@@ -236,7 +237,7 @@ class MultiSelect extends React.Component {
                         flex: 1,
                         fontSize: 14,
                         minHeight: 27,
-                        backgroundColor: 'inherit',
+                        backgroundColor: 'transparent',
                       }),
                     })}
                   />

src/pages/connect/lifegroups/index.js
@@ -14,17 +14,18 @@ import MultiSelect from '../../../components/MultiSelect';
 
 const title = 'LifeGroups';
 
-const Center = styled.div`
+const DatePickerContainer = styled.div`
   text-align: center;
+  margin-top: 40px;
 `;
 
 const CardContainer = styled.div`
   display: grid;
   grid-template-columns: repeat(2, 1fr);
+  gap: 40px;
 
-  > div {
-    width: 500px;
-    max-width: 90%;
+  @media (max-width: 799px) {
+    grid-template-columns: 1fr;
   }
 `;
 
@@ -89,11 +90,14 @@ class Page extends React.Component {
           <Img sizes={data.lifegroupImage.childImageSharp.sizes} />
         </Image>
 
-        <Center>
+        <DatePickerContainer>
           <H3>Which days work best for you?</H3>
-        </Center>
 
-        <MultiSelect onChange={this.handleLifeGroupDayChange} items={allDays} />
+          <MultiSelect
+            onChange={this.handleLifeGroupDayChange}
+            items={allDays}
+          />
+        </DatePickerContainer>
 
         {!!this.state.selectedDays.length && (
           <H4>

src/utils/typography.js
@@ -1,11 +1,11 @@
 import Typography from 'typography';
 import theme from 'typography-theme-bootstrap';
 
-theme.overrideThemeStyles = ({ rhythm }, options, styles) => ({
-  ul: {
-    marginLeft: 0,
-  },
-});
+// theme.overrideThemeStyles = ({ rhythm }, options, styles) => ({
+//   ul: {
+//     marginLeft: 0,
+//   },
+// });
 
 const typography = new Typography(theme);
 const { rhythm, scale } = typography;